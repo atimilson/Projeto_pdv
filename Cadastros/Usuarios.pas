aad89f58048d51229e6c4283ef15751a20d3a2fe
atimilson <atimilson95@gmail.com>
2019-10-03 22:45:00 -0300

    Projeto PDV para estudar a emissao de nota fiscal e relatórios
    Signed-off-by: atimilson <atimilson95@gmail.com>

---
+++ Cadastros/Usuarios.pas
@@ -0,0 +1,24 @@
+unit Usuarios;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TfrmUsuarios = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmUsuarios: TfrmUsuarios;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ __history/PDV.dpr.~3~
@@ -0,0 +1,16 @@
+program PDV;
+
+uses
+  Vcl.Forms,
+  Login in 'Login.pas' {FrmLogin},
+  Menu in 'Menu.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmLogin, FrmLogin);
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
